3f3d41073d498e8dd029b69f05087ad87261544d
semin <semin@example.com>
2025-03-11 22:17:03 +0900

    기본

--- app/src/main/java/com/example/p20/StockViewModel.kt
+++ app/src/main/java/com/example/p20/StockViewModel.kt
@@ -11,18 +11,20 @@ class StockViewModel : ViewModel() {
     val stockItems: LiveData<MutableList<Stock>> get() = _stockItems
 
     init {
-        // 초기 데이터 설정
-        _stockItems.value = mutableListOf(
-            Stock("테슬라", 10000, 0, 0.0, 0),
-            Stock("애플", 10000, 0, 0.0, 0),
-            Stock("아마존", 10000, 0, 0.0, 0),
-            Stock("MS", 10000, 0, 0.0, 0)
-        )
+        // 초기 데이터 설정 (첫 화면에서만 설정)
+        if (_stockItems.value.isNullOrEmpty()) {
+            _stockItems.value = mutableListOf(
+                Stock("테슬라", 10000, 0, 0.0, 0),
+                Stock("애플", 10000, 0, 0.0, 0),
+                Stock("아마존", 10000, 0, 0.0, 0),
+                Stock("MS", 10000, 0, 0.0, 0)
+            )
+        }
     }
 
     // 주식 가격을 3초마다 업데이트하는 함수
     fun updateStockPrices() {
         _stockItems.value?.forEach { it.updateChangeValue() }
-        _stockItems.postValue(_stockItems.value) // postValue로 변경 트리거
+        _stockItems.value = _stockItems.value // LiveData 변경을 트리거
     }
 }
