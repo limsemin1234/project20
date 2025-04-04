b9c39a1ea31dab44a726c2c086f63debb50b4de6
semin <semin@example.com>
2025-04-04 21:41:22 +0900

    부동산 기능 추가

--- app/src/main/java/com/example/p20/SplashActivity.kt
+++ app/src/main/java/com/example/p20/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.p20
 import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,16 +13,17 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // 텍스트뷰 페이드인 애니메이션
-        val fadeIn = AlphaAnimation(0f, 1f)
-        fadeIn.duration = 2000 // 2초 동안 페이드인
-
         val appNameText = findViewById<TextView>(R.id.appNameText)
         val loadingText = findViewById<TextView>(R.id.loadingText)
         val startButton = findViewById<Button>(R.id.startButton)
 
+        // appNameText에 XML 애니메이션 적용
+        val titleAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_title_anim)
+        appNameText.startAnimation(titleAnimation)
+
         // 텍스트 페이드인 애니메이션 시작
-        appNameText.startAnimation(fadeIn)
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.duration = 2000 // 2초 동안 페이드인
         loadingText.startAnimation(fadeIn)
         startButton.startAnimation(fadeIn)
 
