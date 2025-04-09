6b4dffd5ad71ec6a247837428dc2142d704692bc
semin <semin@example.com>
2025-04-09 17:09:02 +0900

    카지노 1인발로란트

--- app/src/main/java/com/example/p20/SplashActivity.kt
+++ app/src/main/java/com/example/p20/SplashActivity.kt
@@ -2,9 +2,10 @@ package com.example.p20
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationUtils
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,7 +16,6 @@ class SplashActivity : AppCompatActivity() {
 
         val appNameText = findViewById<TextView>(R.id.appNameText)
         val loadingText = findViewById<TextView>(R.id.loadingText)
-        val startButton = findViewById<Button>(R.id.startButton)
 
         // appNameText에 XML 애니메이션 적용
         val titleAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_title_anim)
@@ -25,14 +25,12 @@ class SplashActivity : AppCompatActivity() {
         val fadeIn = AlphaAnimation(0f, 1f)
         fadeIn.duration = 2000 // 2초 동안 페이드인
         loadingText.startAnimation(fadeIn)
-        startButton.startAnimation(fadeIn)
 
-        // 게임 시작 버튼 클릭 리스너
-        startButton.setOnClickListener {
-            // 메인 액티비티로 이동
+        // 3초 후에 MainActivity로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        }, 3000)
     }
 } 
